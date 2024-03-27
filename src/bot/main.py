932c947745a387aae36829a2e0ea83a47a3afe97
pydragon <137177056+pydragon1@users.noreply.github.com>
2024-03-28 01:13:28 +0700

    upd

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import json
 from message_printer import MessagePrinter
@@ -7,35 +6,53 @@ import time
 import random
 from config import API_ID, API_HASH, PHONE_NUMBER, PHONE_CODE
 
-    
+
+async def write_file(data_list: list, filename: str="datas"):
+    with open('{filename}.json','w', encoding='utf-8') as f:
+        json.dump(data_list, f, ensure_ascii=False, indent=4)
+
+
 mp = MessagePrinter()
-app = Client("my_account", api_id=API_ID,
+app = Client("my_account", 
+             api_id=API_ID,
              api_hash=API_HASH,
-             phone_code=PHONE_CODE, phone_number=PHONE_NUMBER)
+             phone_code=PHONE_CODE,
+             phone_number=PHONE_NUMBER)
 
-@app.on_message(filters.command("start"))
-async def start_handler(client, message):
-    try:
-        await app.send_message(message.from_user.id,f"Получен /start от пользователя {message.from_user.id}")
-        data = {}
-        chat = await app.get_chat("naebnet")
-        chat_id = chat.id
-        mp.info_message("START")
-        data_list=[]
-        async for posts in app.get_chat_history(chat_id, limit=1000):
-            data["views"] = posts.views
-            data["date"] = str(posts.date)
+
+# @app.on_message(filters.command("start"))
+async def main():
+    data = {}
+    data_list=[]
+    LIMIT = 100
+    async with app:
+        try:
+            await app.send_message('pydrag0n',
+                                f"Получен /start от пользователя 'pydrag0n")
+
+            chat = await app.get_chat("naebnet")
+            chat_id = chat.id
             
-            data_list.append(data)
-            data = {}
-        time.sleep(random.randint(2, 6))
-        with open('datas.json','w', encoding='utf-8') as f:
-            json.dump(data_list, f, ensure_ascii=False, indent=4)
-        
-        mp.info_message("END")
-        
-        
-    except Exception as e:
-        mp.error_message(e)
+            mp.info_message("START")
 
-app.run()
+            async for posts in app.get_chat_history(chat_id, limit=LIMIT):
+                
+                data["views"] = posts.views
+                data["date"] = str(posts.date)
+                
+                data_list.append(data)
+                data = {}
+                
+            time.sleep(random.randint(2, 5))
+            await write_file(data_list)
+            
+            mp.info_message("END")
+            
+            
+        except Exception as e:
+            mp.error_message(e)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
